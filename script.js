beb66e77ddf25c62f56613c64adc066edf49c219
Samuel Poveda <64924802+s-poveda@users.noreply.github.com>
2020-06-05 23:37:14 -0400

    Add files via upload
    Fixed nav. Added <img> for #home

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let greetingMessageElement = document.getElementById('dynamic-greeting');
 const msg = () => {
   const currentHour = new Date().getHours();
-  if (currentHour > 18) return 'Good evening!';
-  if (currentHour >= 12) return 'Good afternoon!';
-  return 'Good morning!'
+  if (currentHour > 18) return 'Good Evening!';
+  if (currentHour >= 12) return 'Good Afternoon!';
+  return 'Good Morning!'
 }
-greetingMessageElement.innerHTML = `<h3><b>${msg()}</b></h3>`;
+greetingMessageElement.innerHTML = `${msg()}`;
