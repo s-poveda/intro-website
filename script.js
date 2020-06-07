0de9c5caf7b40384fff3eefba0ef811f93fa4d89
s-poveda <spoveda545@gmail.com>
2020-06-07 18:38:44 -0400

    Finished

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let greetingMessageElement = document.getElementById('dynamic-greeting');
 const msg = () => {
   const currentHour = new Date().getHours();
-  if (currentHour > 18) return 'Good Evening!';
-  if (currentHour >= 12) return 'Good Afternoon!';
-  return 'Good Morning!'
+  if (currentHour > 18) return 'Good Evening,';
+  if (currentHour >= 12) return 'Good Afternoon,';
+  return 'Good Morning,'
 }
-greetingMessageElement.innerHTML = `${msg()}`;
+greetingMessageElement.innerHTML = `${msg()} I'm Sam!`;
