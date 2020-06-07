070055406c5b6c13343165fd241a3954960c7131
s-poveda <spoveda545@gmail.com>
2020-06-07 19:08:07 -0400

    Finished

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let greetingMessageElement = document.getElementById('dynamic-greeting');
+
+//Msg checks the current time (0 - 24) and return a greeting message
 const msg = () => {
   const currentHour = new Date().getHours();
   if (currentHour > 18) return 'Good Evening,';
